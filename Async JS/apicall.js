3e146eeebdf8e31e3e106e0d5fb16081359054de
Rajat Upadhyay <rajatis1999@gmail.com>
2021-08-04 19:46:20 +0530

    making api call

---
+++ Async JS/apicall.js
@@ -0,0 +1,14 @@
+
+const callApi = async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+callApi().then(res => {
+    console.log(res);
+}).catch(err => {
+    console.log(err);
+});
