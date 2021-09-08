6bbe94c9f52c9dc5e50a6844d042758ad676046b
Rajat Upadhyay <rajatis1999@gmail.com>
2021-09-08 18:44:24 +0530

    middleware to throw error

--- Notification API/app.js
+++ Notification API/app.js
@@ -1,31 +1,37 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const axios = require('axios');
 app.use(express.json());
 
-app.post('/notification', async(req, res) => {
+const polling_url = 'http://127.0.0.1:5000/polling';
+
+async function middleWare(err, req, res, next){
+    console.log(err);
+    return res.send({"statusCode":500, "msg":"Some error occured"});
+}
+
+app.post('/notification', async(req, res, next) => {
     try{
         let {data} = req.body;
-        console.log(data);
+        console.log(data.ehllo);
         return res.send({statusCode:200, msg:"Data Received"});
     }
     catch(err)
     {
-        console.log(err);
-        return res.send({statusCode:500, msg:"Some error occured"});
+        next(err);
     }
 });
 
-app.get('/status', async(req, res) => {
-    await axios.get('http://127.0.0.1:5000/polling').then((response) => {
-        // console.log(response)
+app.get('/status', async(req, res, next) => {
+    await axios.get(polling_url).then((response) => {
         return res.send(response.data);
     })
     .catch(err => {
         console.log(err);
     });
 });
+
+app.use(middleWare);
 
 app.listen(3000,function(){
     console.log("Server is running.");
